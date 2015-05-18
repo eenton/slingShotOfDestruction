3d61d32f0a26435eabaf50054f4c8e79dfecf5f6
anton und marius <anton.boger@web.de>
2015-05-18 16:08:58 +0200

    sling shot scripts

---
+++ Assets/scripts/slingshot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slingshot : MonoBehaviour {
+	public GameObject launchPoint;
+	void Awake(){
+		Transform launchPointTrans = transform.Find("launchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+
+
+	void OnMouseEnter(){
+
+
+			launchPoint.SetActive (true);
+
+
+	}
+void OnMouseExit(){
+
+			launchPoint.SetActive (false);
+	
+	}
+
+	
+	
+
+}
